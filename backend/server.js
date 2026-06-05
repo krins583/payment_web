b37416aa0817fdc74f8599e875666cca65c25ccb
Krins Sutariya <krins.sutariya@example.com>
2026-06-05 17:17:15 +0530

    Fixed port for Render

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ app.get("/api/history", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
   console.log("🔥 Firebase Connected successfully!");
